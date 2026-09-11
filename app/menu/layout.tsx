4304bd6abbe41bc5df0a8a034f06a14f29003120
priy-29 <maspri2904@gmail.com>
2026-09-11 20:10:54 +0700

    fix: reserve space for mobile customer navigation

--- app/menu/layout.tsx
+++ app/menu/layout.tsx
@@ -2,5 +2,5 @@ import type { ReactNode } from "react";
 import PublicNav from "../../components/PublicNav";
 
 export default function MenuLayout({ children }: { children: ReactNode }) {
-  return <><PublicNav/><div className="lg:pl-[230px]">{children}</div></>;
+  return <><PublicNav/><div className="pb-24 lg:pb-0 lg:pl-[230px]">{children}</div></>;
 }
